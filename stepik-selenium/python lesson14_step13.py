8af5a97754142e3164f8dd7c1f0d0043a4712489
evgsaf <evgenii.safarov@steor.tech>
2021-11-03 12:36:04 +0300

    add stepik excersise

---
+++ stepik-selenium/python lesson14_step13.py
@@ -0,0 +1,83 @@
+from selenium import webdriver
+import time
+
+import unittest
+
+
+class TestReg(unittest.TestCase):
+    def test_reg1(self):
+        try:
+            link = "http://suninjuly.github.io/registration1.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            # Ваш код, который заполняет обязательные поля .first_block input
+            elements = (
+                browser.find_element_by_xpath(
+                    f"//div[@class='first_block']//label[text()='{field}']/following-sibling::input") for
+                field in ("First name*", "Last name*", "Email*"))
+            for element in elements:
+                element.send_keys("WTF")
+
+            # Отправляем заполненную форму
+            button = browser.find_element_by_css_selector("button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element_by_tag_name("h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text, 'reg1 should be ok')
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(10)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+    def test_reg2(self):
+        try:
+            link = "http://suninjuly.github.io/registration2.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            # Ваш код, который заполняет обязательные поля .first_block input
+            elements = (
+                browser.find_element_by_xpath(
+                    f"//div[@class='first_block']//label[text()='{field}']/following-sibling::input") for
+                field in ("First name*", "Last name*", "Email*"))
+            for element in elements:
+                element.send_keys("WTF")
+
+            # Отправляем заполненную форму
+            button = browser.find_element_by_css_selector("button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element_by_tag_name("h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text, 'reg2 should be ok')
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(10)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
