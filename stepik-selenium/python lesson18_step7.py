2797d912633f16291766c806908f1624cf5ac5e1
evgsaf <evgenii.safarov@steor.tech>
2021-11-08 13:43:32 +0300

    add stepik excersise

---
+++ stepik-selenium/python lesson18_step7.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+import time
+import math
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#magic_link")
